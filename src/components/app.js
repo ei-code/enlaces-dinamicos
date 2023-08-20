d4ec446eed520fbfd7f1aae0503e83ef3a51db46
ei <eiderastrain@gmail.com>
2023-08-20 12:48:09 +0200

    enlaces

---
+++ src/components/app.js
@@ -0,0 +1,117 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import axios from "axios";
+import { Home } from "./pages/home";
+import { Contact } from "./pages/contact";
+import { Admin } from "./pages/admin";
+import NoMatch from "./pages/no-match";
+import Auth from "./pages/auth";
+import NavigationComponent from "./navigation-container";
+
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loggedInStatus: "NOT_LOGGED-IN",
+    };
+    this.handleSuccessfulLogin = this.handleSuccessfulLogin.bind(this);
+    this.handleUnsuccessfulLogin = this.handleUnsuccessfulLogin.bind(this);
+    this.handleSuccessfulLogout = this.handleSuccessfulLogout.bind(this);
+  }
+  handleSuccessfulLogin() {
+    this.setState({
+      loggedInStatus: "LOGGED_IN",
+    });
+  }
+  handleUnsuccessfulLogin() {
+    this.setState({
+      loggedInStatus: "NOT_LOGGED_IN",
+    });
+  }
+  handleSuccessfulLogout() {
+    this.setState({
+      loggedInStatus: "NOT_LOGGED_IN",
+    });
+  }
+  checkLoginStatus() {
+    return axios
+      .get("https://api.devcamp.space/logged_in", {
+        withCredentials: true,
+      })
+      .then((response) => {
+        console.log("logged_in response", response);
+        const loggedIn = response.data.logged_in;
+        const loggedInStatus = this.state.loggedInStatus;
+
+        // If loggedIn and status LOGGED_IN => return data
+        // If loggedIn status NOT_LOGGED_IN => update state
+        // If not loggedIn and status LOGGED_IN => update state
+
+        if (loggedIn && loggedInStatus === "LOGGED_IN") {
+          return loggedIn;
+        } else if (loggedIn && loggedInStatus === "NOT_LOGGED_IN") {
+          this.setState({
+            loggedInStatus: "LOGGED_IN",
+          });
+        } else if (!loggedIn && loggedInStatus === "LOGGED_IN") {
+          this.setState({
+            loggedInStatus: "NOT_LOGGED_IN",
+          });
+        }
+      })
+      .catch((error) => {
+        console.log("Error", error);
+      });
+  }
+
+  componentDidMount() {
+    this.checkLoginStatus();
+  }
+
+  authorizadPages() {
+    return [<Route key="admin" exact path="/admin" component={Admin} />];
+  }
+  render() {
+    return (
+      <div className="App">
+        <div className="loggedOrNot">
+          <h2>{this.state.loggedInStatus}</h2>
+        </div>
+        <Router>
+          <div>
+            <NavigationComponent
+              loggedInStatus={this.state.loggedInStatus}
+              handleSuccessfulLogout={this.handleSuccessfulLogout}
+            />
+
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/contact" component={Contact} />
+              <Route
+                path="/auth"
+                render={(props) => (
+                  <Auth
+                    {...props}
+                    handleSuccessfulLogin={this.handleSuccessfulLogin}
+                    handleUnsuccessfulLogin={this.handleUnsuccessfulLogin}
+                  />
+                )}
+              />
+              {this.state.loggedInStatus === "LOGGED_IN"
+                ? this.authorizadPages()
+                : null}
+
+              {/*this.state.loggedInStatus === "LOGGED_IN" && (
+                <Route exact path="/admin" component={Admin} />
+              )*/}
+
+              <Route component={NoMatch} />
+            </Switch>
+          </div>
+        </Router>
+      </div>
+    );
+  }
+}
